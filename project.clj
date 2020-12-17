08d759189b69f11c24527d7af8a3dcdfad78b4a5
Maurício Szabo <mauricio.szabo@gmail.com>
2020-12-17 10:50:27 -0300

    Project update

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject pathom-smart-maps "0.1.0-SNAPSHOT"
+(defproject pathom-smart-maps "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns pathom-smart-maps.core})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.wsscode/pathom "2.3.0-alpha17"]]
+  :repl-options {:init-ns user})
