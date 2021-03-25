1cec4990256f436dca3b8eb723992ef8f83e11b9
Maurício Szabo <mauricio.szabo@gmail.com>
2021-03-25 10:57:14 -0300

    Trying to fix the need for atoms

--- src/pathom_smart_maps/async.cljs
+++ src/pathom_smart_maps/async.cljs
@@ -18,7 +18,11 @@
                   p/error-handler-plugin
                   p/trace-plugin]}))
 
-(def SmartMap (js* "class SmartMap extends Promise {}"))
+(def SmartMap (js* "class SmartMap extends Promise {
+  __state_then(fn) {
+    return super.then(fn)
+  }
+}"))
 
 (defn- norm-cache [cache]
   (atom
@@ -27,19 +31,6 @@
      (nil? cache) {}
      (instance? Atom cache) @cache
      :else cache)))
-
-(defn- state-from-params
-  [{:keys [cache cursor req-cache not-found-cache env] :as params}]
-  (c/let [req-cache (norm-cache req-cache)
-          cache (norm-cache cache)]
-    (assoc params
-           :env (assoc env
-                       ::p/entity cache
-                       ::p/request-cache req-cache)
-           :cursor (or cursor [])
-           :cache cache
-           :req-cache req-cache
-           :not-found-cache (or not-found-cache #{}))))
 
 (defn- ->SmartMap [{:keys [cache cursor req-cache not-found-cache env] :as params}]
   (c/let [req-cache (norm-cache req-cache)
@@ -51,53 +42,52 @@
                        :cursor (or cursor [])
                        :cache cache
                        :req-cache req-cache
-                       :not-found-cache (or not-found-cache #{}))
-          state (atom state)]
-    (doto (. SmartMap resolve state)
-          (aset "_state" state))))
+                       :not-found-cache (atom (or not-found-cache #{})))]
+          ; state (atom state)]
+    (def s state)
+    (. SmartMap resolve state)))
+    ; (doto
+    ;       (aset "_state" state))))
 
 (defn- compare-to [^js this other]
   (if (instance? SmartMap other)
     (= (.-_state this) (.-_state other))
     (= (.-_state this) other)))
 
-(defn- wrap-result [^js sm [key cached-value]]
-  (c/let [state @(.-_state sm)]
-    (->SmartMap (update state :cursor conj key))))
+; (defn- wrap-result [^js sm [key cached-value]]
+;   (c/let [state (.-_state sm)]
+;     (->SmartMap (update state :cursor conj key))))
 
 (declare sm-get)
-(defn- make-query! [^js sm k default]
-  (c/let [state (.-_state sm)
-          {:keys [parser env]} @state]
-    (-> (SmartMap.
+(defn- make-query! [state k default]
+  (c/let [{:keys [parser env]} state]
+    (-> (js/Promise.
          (fn [resolve reject]
            (async/go
              ;; FIXME - error case
              (c/let [res (async/<! (parser env [k]))]
                (if (#{::p/not-found ::p/reader-error} (get res k ::p/not-found))
-                 (swap! state update :not-found-cache conj k)
-                 (swap! (:cache @state) merge res))
-               (resolve state)))))
-        (doto (aset "_state" state))
-        (.then #(sm-get sm k default)))))
+                 (swap! (:not-found-cache state) conj k)
+                 (swap! (:cache state) merge res))
+               (resolve (sm-get state k default)))))))))
+               ; (resolve state)))]))
+        ; (.__state_then #(sm-get % k default))))
 
-(defn- sm-get [^js sm k default]
-  (c/let [state (.-_state sm)
-          {:keys [not-found-cache cache eql cursor]} @state
+(defn- sm-get [state k default]
+  (c/let [{:keys [not-found-cache cache eql cursor]} state
           cache (get-in @cache cursor)
           cached (find cache k)]
     (cond
-      ;; FIXME: check if this should be a Promise
-      cached (wrap-result sm cached)
-      (contains? not-found-cache k) default
-      :else (make-query! sm k default))))
+      cached (update state :cursor conj (first cached))
+      (contains? @not-found-cache k) (assoc state :final-val default)
+      :else (make-query! state k default))))
 
-(extend-protocol ILookup
-  SmartMap
-  (-lookup
-   ([this k] (sm-get this k nil))
-   ([this k not-found] (sm-get this k not-found))))
-
+; (extend-protocol ILookup
+;   SmartMap
+;   (-lookup
+;    ([this k] (.__state_then this #(doto (sm-get % k nil) (prn ::LOL))))
+;    ([this k not-found] (.__state_then this #(sm-get % k not-found)))))
+;
 (defn- dissoc-children [cache idx-info key]
   (c/let [resolvers (get-in idx-info [::pc/index-attributes key ::pc/attr-input-in])
           children (mapcat #(get-in idx-info [::pc/index-resolvers % ::pc/output])
@@ -121,138 +111,156 @@
               (dissoc-req-cache acc idx-info new-key))
             new-req-cache children)))
 
-(defn- sm-dissoc [^js this k]
-  (c/let [state @(.-_state this)
-          old-cache @(:cache state)
+(defn- sm-dissoc [state k]
+  (c/let [old-cache @(:cache state)
           old-req-cache @(:req-cache state)
           idx-info (:idx-info state)]
     (assoc state
            :cache (dissoc-children old-cache idx-info k)
            :req-cache (dissoc-req-cache old-req-cache idx-info k))))
 
-(defn- sm-assoc [^js this k v]
-  (c/let [new-state (sm-dissoc this k)]
+(defn- sm-assoc [state k v]
+  (sm-dissoc state k)
+  #_
+  (c/let [new-state (sm-dissoc state k)]
+    ; (prn :NEW)
+    ; (def new-state new-state)
+    ; (def k k)
+    ; (def v v)
+    ; (prn :NEW (update new-state :cache assoc k v))
+    (prn :NEW?)
     (->SmartMap (update new-state :cache assoc k v))))
 
-(defn- get-keys-dependencies [state-atom]
-  (c/let [state @state-atom]
-    (-> state :idx-info ::pc/index-oir)))
+; (:cache new-state)
 
-(defn- sm-contains? [^js this, k]
-  (c/let [state-atom (.-_state this)
-          state @state-atom]
-    (if (contains? (:not-found-cache state) k)
-      false
-      (-> state-atom get-keys-dependencies (contains? k)))))
+(defn- get-keys-dependencies [state]
+  (-> state :idx-info ::pc/index-oir))
 
-(defn- sm-seq [^js this]
-  (c/let [not-found (js/Object.)]
-    (->> this
-         .-_state
-         deref
-         :cache
-         deref
-         seq)))
+(defn- sm-contains? [state k]
+  (if (contains? @(:not-found-cache state) k)
+    false
+    (-> state get-keys-dependencies (contains? k))))
+;
+; (defn- sm-seq [^js this]
+;   (some-> this
+;           .-_state
+;           :cache
+;           deref
+;           seq))
+;
+; (defn- sm-find [this k])
+  ; (c/let [not-found (js/Object.)]
+  ;   (when (-contains-key? this k)
+  ;     (.then (get this k not-found) (fn [v]
+  ;                                     (if (= not-found v)
+  ;                                       nil
+  ;                                       (MapEntry. k v (hash [k v]))))))))
 
-(defn- sm-find [this k]
-  (c/let [not-found (js/Object.)]
-    (when (-contains-key? this k)
-      (.then (get this k not-found) (fn [v]
-                                      (if (= not-found v)
-                                        nil
-                                        (MapEntry. k v (hash [k v]))))))))
+; (defn- sm-doall [^js this]
+;   (c/let [not-found (js/Object.)
+;           kvs-prom (->> this
+;                         .-_state
+;                         get-keys-dependencies
+;                         keys
+;                         (map (fn [k]
+;                                (-> this
+;                                    (get k not-found)
+;                                    (.then (fn [v] [k v])))))
+;                         (js/Promise.all))]
+;     (.then kvs-prom (fn [kvs] (remove #(-> % second (= not-found)) kvs)))))
 
-(defn- sm-doall [^js this]
-  (c/let [not-found (js/Object.)
-          kvs-prom (->> this
-                        .-_state
-                        get-keys-dependencies
-                        keys
-                        (map (fn [k]
-                               (-> this
-                                   (get k not-found)
-                                   (.then (fn [v] [k v])))))
-                        (js/Promise.all))]
-    (.then kvs-prom (fn [kvs] (remove #(-> % second (= not-found)) kvs)))))
-
-(defn- sm-reduce [sequence f seed]
-  (reduce (fn [acc-prom [key val]]
-            (.then acc-prom
-                   (fn [acc]
-                     (c/let [entry (MapEntry. key val (hash [key val]))]
-                       (f acc entry)))))
-          (js/Promise.resolve seed)
-          sequence))
+; (defn- sm-reduce [sequence f seed]
+;   (reduce (fn [acc-prom [key val]]
+;             (.then acc-prom
+;                    (fn [acc]
+;                      (c/let [entry (MapEntry. key val (hash [key val]))]
+;                        (f acc entry)))))
+;           (js/Promise.resolve seed)
+;           sequence))
 
 (defn- sm-then [old-state fun]
-  (c/let [old-state @old-state
-          cache (:cache old-state)
-          value (:final-val old-state (get-in @cache (:cursor old-state)))]
-    (fun value)))
+  ; (tap> [:old-state old-state])
+  (c/let [cache (:cache old-state)
+          value (:final-val old-state (get-in @cache (:cursor old-state)))
+          ; _ (prn :cursor (:cursor old-state)
+          ;        :from-cache (get-in @cache (:cursor old-state))
+          ;        :final-val (:final-val old-state))
+          res (fun value)]
+
+    ; (prn :res res
+    ;      :promise? (instance? js/Promise res)
+    ;      :smart? (instance? SmartMap res))
+
+    (assoc old-state :final-val res)))
+
+(defn- sm-first [^js this]
+  #_
+  (.__state_then this
+         #(prn :STATEA (-> @% :path))))
+  ; (c/let [state (.-_state this)]
+  ;   (def st state)
+  ;   []))
 
 (extend-type SmartMap
   Object
   (equiv [this other] (compare-to this other))
-  (then [this fun]
-    (c/let [super (.. js/Promise -prototype -then (bind this))
-            state-var (atom nil)
-            res-super
-            (super (fn [state]
-                     (reset! state-var state)
-                     (sm-then state fun)))
-            other-super (.. js/Promise -prototype -then (bind res-super))]
-      (other-super (fn [elem]
-                     (-> @@state-var
-                         (assoc :final-val elem)
-                         atom)))))
+  (then [this fun] (.__state_then this #(sm-then % fun)))
 
-  IEquiv
-  (-equiv [this other] (compare-to this other))
-
+  ILookup
+  (-lookup
+   ([this k] (.__state_then this #(sm-get % k nil)))
+   ([this k not-found] (.__state_then this #(sm-get % k not-found))))
+  ; IEquiv
+  ; (-equiv [this other] (compare-to this other))
+  ;
   IAssociative
-  (-assoc [this k v] (sm-assoc this k v))
-  (-contains-key? [this k] (sm-contains? this k))
-
-  ICollection
-  (-conj [this [k v]] (sm-assoc this k v))
-
-  IMap
-  (-dissoc [this k] (->SmartMap (sm-dissoc this k)))
-
-  IEmptyableCollection
-  (-empty [this] (c/let [state @(.-_state this)]
-                   (->SmartMap (assoc state :cache {} :req-cache {} :not-found-cache #{}))))
-
-  IReduce
-  (-reduce
-   ([this f] (-> this sm-doall (.then (fn [[fst & rst]] (sm-reduce rst f fst)))))
-   ([this f seed] (-> this sm-doall (.then (fn [seq] (sm-reduce seq f seed))))))
-
-  IKVReduce
-  (-kv-reduce [this f init]
-              (-> this
-                  sm-doall
-                  (.then #(sm-reduce % (fn [acc [k v]] (f acc k v)) init))))
-
-  ISeqable
-  (-seq [this] (sm-seq this))
-
-  ICloneable
-  (-clone [this] (-> @(.-_state this)
-                     (update :cache deref)
-                     (update :req-cache deref)
-                     ->SmartMap))
-
-  IWithMeta
-  (-with-meta [this new-meta] (-> @(.-_state this)
-                                  (assoc :meta new-meta)
-                                  ->SmartMap))
-
-  IMeta
-  (-meta [this] (:meta @(.-_state this)))
-
-  IFind
-  (-find [this k] (sm-find this k)))
+  (-assoc [this k v] (.__state_then this #(sm-assoc % k v)))
+  (-contains-key? [this k] (.__state_then this #(sm-contains? % k))))
+  ;
+  ; ICollection
+  ; (-conj [this [k v]] (sm-assoc this k v))
+  ;
+  ; IMap
+  ; (-dissoc [this k] (->SmartMap (sm-dissoc this k)))
+  ;
+  ; IEmptyableCollection
+  ; (-empty [this] (c/let [state @(.-_state this)]
+  ;                  (->SmartMap (assoc state :cache {} :req-cache {} :not-found-cache #{}))))
+  ;
+  ; IReduce
+  ; (-reduce
+  ;  ([this f] (-> this sm-doall (.then (fn [[fst & rst]] (sm-reduce rst f fst)))))
+  ;  ([this f seed] (-> this sm-doall (.then (fn [seq] (sm-reduce seq f seed))))))
+  ;
+  ; IKVReduce
+  ; (-kv-reduce [this f init]
+  ;             (-> this
+  ;                 sm-doall
+  ;                 (.then #(sm-reduce % (fn [acc [k v]] (f acc k v)) init))))
+  ;
+  ; ISeqable
+  ; (-seq [this] (sm-seq this))
+  ;
+  ; ICloneable
+  ; (-clone [this] (-> (.-_state this)
+  ;                    (update :cache deref)
+  ;                    (update :req-cache deref)
+  ;                    ->SmartMap))
+  ;
+  ; IWithMeta
+  ; (-with-meta [this new-meta] (-> (.-_state this)
+  ;                                 (assoc :meta new-meta)
+  ;                                 ->SmartMap))
+  ;
+  ; IMeta
+  ; (-meta [this] (:meta (.-_state this)))
+  ;
+  ; IFind
+  ; (-find [this k] (sm-find this k))
+  ;
+  ; ISeq
+  ; (-first [this] (sm-first this))
+  ; (^clj -rest [coll]))
 
 (defn smart-map
   ([resolvers] (smart-map resolvers {}))
--- test/pathom_smart_maps/async_test.cljs
+++ test/pathom_smart_maps/async_test.cljs
@@ -28,131 +28,155 @@
   (swap! calls conj :an-error)
   (throw (ex-info "That's an error. That's all" {})))
 
-(def smap (smart/smart-map [root-person]))
 #_(-> ;smap
       (smart/smart-map [root-person])
       :person/sn
-      (.then #(do (prn :FINAL-RES %) (+ % 20)))
-      (.then #(do (prn :FINAL-RES-2 %) 20))
-      (.then #(do (prn :FINAL-RES-3 %) 30))
-      type)
+      (.then #(do (prn :FINAL-RES %) (+ % 20))))
+      ; (.then #(do (prn :FINAL-RES-2 %) 20))
+      ; (.then #(do (prn :FINAL-RES-3 %) 30)))
+      ; type)
       ; (do nil))
-
-#_
-(let [s (.. js/Promise -prototype -then
-            (bind smap))]
-  (s (fn [a] (prn :A a))))
 
 (deftest root-resolver
   (async-test "resolving roots"
     (let [map (smart/smart-map [root-person])]
       (check map => {})
 
-
       (testing "gets the attributes, makes a call to EQL"
         (check (:person/gn map) => "Name")
         (check @calls => [:root-person]))
 
-      #_
       (testing "gets the attributes, does not make a call to EQL"
         (check (:person/gn map) => "Name")
+        (check @calls => [:root-person]))
+
+      (testing "don't find unexisting attribute, keep default"
+        (check (:person/my-name map ::name) => ::name)
         (check @calls => [:root-person])))))
 
-; (deftest destructuring-test
-;   (let [smart (smart/smart-map [root-person full-name])]
-;     (async-test "destructuring fields in promises"
-;       (smart/let [{:person/keys [gn]} smart]
-;         (check gn => "Name")))))
-;
-; (deftest sub-resolvers
-;   (let [smart (smart/smart-map [root-person full-name])]
-;     (reset! calls [])
-;     (async-test "resolving a data that depends on another"
-;       (testing "will get the dependent data"
-;         (check (:person/full-name smart) => "Name Surname")
-;         (check @calls => [:root-person :full-name])
-;
-;        (testing "will cache all information"
-;          (check smart => {:person/gn "Name"
-;                           :person/sn "Surname"
-;                           :person/full-name "Name Surname"})
-;          (check @calls => [:root-person :full-name])))
-;
-;       (testing "un-caches results if you assoc a parent data"
-;         (check (:person/full-name (assoc smart :person/gn "Other"))
-;                => "Other Surname")
-;         (check @calls => [:root-person :full-name :full-name]))
-;
-;       (testing "un-caches results if you conj a parent data"
-;         (check (:person/full-name (conj smart [:person/gn "Another"]))
-;                => "Another Surname")
-;         (check @calls => [:root-person :full-name :full-name :full-name]))
-;
-;       (testing "un-cache results if you dissoc some data"
-;         (check (:person/full-name (dissoc smart :person/full-name))
-;                => "Name Surname")
-;         (check @calls => [:root-person :full-name :full-name
-;                           :full-name :full-name])
-;
-;         (check (:person/full-name (dissoc smart :person/gn))
-;                => "Name Surname")
-;         (check @calls => [:root-person :full-name :full-name
-;                           :full-name :full-name
-;                           :root-person :full-name]))
-;
-;       (testing "behaves like a normal ClojureScript map"
-;         (check (empty smart) => {})
-;         (check (clone smart) => smart)))))
-;
-; (deftest containing-errors []
-;   (let [smart (smart/smart-map [root-person an-error])]
-;     (reset! calls [])
-;     (async-test "when some resolver gets an error"
-;       (testing "will check if a key is not part of resolvers"
-;         (check (-contains-key? smart :inexistent/field) => false)
-;         (check @calls => []))
-;
-;       (testing "will capture errors"
-;         (check (:error/field smart) => nil)
-;         (check @calls => [:an-error]))
-;
-;       (testing "will cache errors"
-;         (check (:error/field smart) => nil)
-;         (check @calls => [:an-error])
-;         (check (-contains-key? smart :error/field) => false)))))
-;
-; (deftest assoc-ing
-;   (let [smart (-> [root-person full-name]
-;                   smart/smart-map
-;                   (assoc :person/gn "pre-added"))]
-;     (async-test "will cache some of the data into the pipeline"
-;       (check (:person/full-name smart) => "pre-added Surname"))))
-;
-; (deftest non-resolved-entities
-;   (let [smart (smart/smart-map [root-person full-name])]
-;     (async-test "will consider entities that WILL be resolved, but are not right now"
-;       (testing "implements find correctly"
-;         (check (find smart :person/invalid) => nil)
-;         (check (find smart :person/gn) => [:person/gn "Name"]))
-;
-;       (testing "reduce will only resolve on the end"
-;         (let [mapped (reduce (fn [acc [k v]] (str acc " | " k " - " v))
-;                              ""
-;                              (smart/smart-map [root-person]))]
-;           (check mapped => " | :person/gn - Name | :person/sn - Surname"))
-;
-;         (let [mapped (reduce-kv (fn [acc k v]
-;                                   (str acc " | " k " - " v))
-;                              ""
-;                              (smart/smart-map [root-person]))]
-;           (check mapped => " | :person/gn - Name | :person/sn - Surname")))
-;
-;       #_
-;       (testing "map will only resolve on the end"
-;         (let [mapped (map (fn [[k v]] (str k " - " v)) (smart/smart-map [root-person]))]
-;           (check (p/all mapped) => (m/in-any-order [":person/gn - Name"
-;                                                     ":person/sn - Surname"])))))))
-;
+(deftest destructuring-test
+  (let [smart (smart/smart-map [root-person full-name])]
+    (async-test "destructuring fields in promises"
+      (smart/let [{:person/keys [gn]} smart]
+        (check gn => "Name")))))
+
+(def smart (smart/smart-map [root-person full-name]))
+
+#_
+(do
+  (.then (:person/name smart ::LOL) #(prn :not-found %))
+  nil)
+
+#_
+(-> (smart/smart-map [root-person full-name])
+    (assoc :person/gn "Other"))
+    ; (.then #(prn :LOL %)))
+
+(deftest sub-resolvers
+  (let [smart (smart/smart-map [root-person full-name])]
+    (reset! calls [])
+    (async-test "resolving a data that depends on another"
+      #_
+      (testing "will get the dependent data"
+        (check (:person/full-name smart) => "Name Surname")
+        (check @calls => [:root-person :full-name]))
+
+      #_
+      (testing "will cache all information"
+        (check smart => {:person/gn "name"
+                         :person/sn "surname"
+                         :person/full-name "name surname"})
+        (check @calls => [:root-person :full-name]))
+
+      #_
+      (testing "will dissoc info (and un-cache)"
+        (-> smart (dissoc :person/gn) :person/full-name
+            (check => "name surname"))
+        ; (check smart => {:person/gn "name"
+        ;                  :person/sn "surname"
+        ;                  :person/full-name "name surname"})
+        (check @calls => [:root-person :full-name :root-person :full-name]))
+
+      #_
+      (testing "un-caches results if you assoc a parent data"
+        (check (:person/full-name (assoc smart :person/gn "Other"))
+               => "Other Surname")
+        (check @calls => [:root-person :full-name :full-name]))
+      ;
+      ; (testing "un-caches results if you conj a parent data"
+      ;   (check (:person/full-name (conj smart [:person/gn "Another"]))
+      ;          => "Another Surname")
+      ;   (check @calls => [:root-person :full-name :full-name :full-name]))
+      ;
+      ; (testing "un-cache results if you dissoc some data"
+      ;   (check (:person/full-name (dissoc smart :person/full-name))
+      ;          => "Name Surname")
+      ;   (check @calls => [:root-person :full-name :full-name
+      ;                     :full-name :full-name])
+      ;
+      ;   (check (:person/full-name (dissoc smart :person/gn))
+      ;          => "Name Surname")
+      ;   (check @calls => [:root-person :full-name :full-name
+      ;                     :full-name :full-name
+      ;                     :root-person :full-name]))
+      ;
+      ; (testing "behaves like a normal ClojureScript map"
+      ;   (check (empty smart) => {})
+      ;   (check (clone smart) => smart))
+      ,)))
+
+#_
+(deftest containing-errors []
+  (let [smart (smart/smart-map [root-person an-error])]
+    (reset! calls [])
+    (async-test "when some resolver gets an error"
+      (testing "will check if a key is not part of resolvers"
+        (check (-contains-key? smart :inexistent/field) => false)
+        (check @calls => []))
+
+      (testing "will capture errors"
+        (check (:error/field smart) => nil)
+        (check @calls => [:an-error]))
+
+      (testing "will cache errors"
+        (check (:error/field smart) => nil)
+        (check @calls => [:an-error])
+        (check (-contains-key? smart :error/field) => false)))))
+
+#_
+(deftest assoc-ing
+  (let [smart (-> [root-person full-name]
+                  smart/smart-map
+                  (assoc :person/gn "pre-added"))]
+    (async-test "will cache some of the data into the pipeline"
+      (check (:person/full-name smart) => "pre-added Surname"))))
+
+#_
+(deftest non-resolved-entities
+  (let [smart (smart/smart-map [root-person full-name])]
+    (async-test "will consider entities that WILL be resolved, but are not right now"
+      (testing "implements find correctly"
+        (check (find smart :person/invalid) => nil)
+        (check (find smart :person/gn) => [:person/gn "Name"]))
+
+      (testing "reduce will only resolve on the end"
+        (let [mapped (reduce (fn [acc [k v]] (str acc " | " k " - " v))
+                             ""
+                             (smart/smart-map [root-person]))]
+          (check mapped => " | :person/gn - Name | :person/sn - Surname"))
+
+        (let [mapped (reduce-kv (fn [acc k v]
+                                  (str acc " | " k " - " v))
+                             ""
+                             (smart/smart-map [root-person]))]
+          (check mapped => " | :person/gn - Name | :person/sn - Surname")))
+
+      #_
+      (testing "map will only resolve on the end"
+        (let [mapped (map (fn [[k v]] (str k " - " v)) (smart/smart-map [root-person]))]
+          (check (p/all mapped) => (m/in-any-order [":person/gn - Name"
+                                                    ":person/sn - Surname"])))))))
+
 (pc/defresolver children [_ _]
   {::pc/input #{}
    ::pc/output [{:person/children [:person/full-name :person/age]}]}
@@ -165,16 +189,17 @@
 
   {:person/next-age (inc age)})
 
-; (deftest nested-smart-maps
-;   (let [smart (smart/smart-map [children birthday])]
-;     (async-test "will consider some smart-maps results as other smart-maps"
-;       (testing "captures nested map"
-;         (check (:person/children smart)
-;                => (m/equals [{:person/full-name "Child One" :person/age 9}
-;                              {:person/full-name "Child Two" :person/age 12}])))
-;
-;       (testing "resolves birthday for the first child only"
-;         (check (-> smart empty :person/children first :person/next-age) => 10)
+#_
+(deftest nested-smart-maps
+  (let [smart (smart/smart-map [children birthday])]
+    (async-test "will consider some smart-maps results as other smart-maps"
+      (testing "captures nested map"
+        (check (:person/children smart)
+               => (m/equals [{:person/full-name "Child One" :person/age 9}
+                             {:person/full-name "Child Two" :person/age 12}])))
+
+      (testing "resolves birthday for the first child only"
+        (check (-> smart empty :person/children first :person/next-age) => 10)))))
 ;         (check (-> smart empty :person/children first :person/next-age) => 10)
 ;         #_
 ;         (check (:person/children smart)
@@ -185,7 +210,13 @@
 (def smart-nested (smart/smart-map [root-person children birthday]))
 ;
 #_
-(-> smart-nested :person/children first #_:person/next-age)
+(-> (smart/smart-map [root-person children birthday])
+    :person/children
+    ; (.then prn)
+    first)
+    ; count)
+    ; first
+    ; #_:person/next-age)
 ;
 ; #_
 ; (-> smart :person/children #_first #_:person/next-age)
